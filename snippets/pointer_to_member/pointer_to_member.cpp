ba5491a6a5e82f52a6a90540904208b7f1ab03a6
valteem <val.teem.scn@gmail.com>
2023-02-24 16:30:29 +0300

    elaborated pointer-to-member example

--- snippets/pointer_to_member/pointer_to_member.cpp
+++ snippets/pointer_to_member/pointer_to_member.cpp
@@ -20,11 +20,23 @@ class Position2{
     Position2(Node*_v):v(_v){}; 
 };
 
+class Position3{
+    public:
+    Node v;
+    Position3(Node _v):v(_v){};
+};
+
 int main() {
     Node n = { 3 };
     Position p = Position(&n);
-    std::cout << n.elt << std::endl << p.v->elt << std::endl << *p << std::endl;
+    std::cout << "Accessing struct member:                                          " << n.elt << std::endl;
+    std::cout << "Dereferencing class member defined as pointer:                    " << p.v->elt << std::endl; 
+    std::cout << "Accessing class member via overloaded pointer:                    " << *p << std::endl;
     Position2 p2 = Position2(&n);
-    std::cout << n.elt << std::endl << p2.v->elt << std::endl; // << *p2 << std::endl; // no match for operator*
+    std::cout << "Dereferencing class member defined as pointer via arrow operator: "  << p2.v->elt << std::endl;
+    std::cout << "Explicit dereferencing class member defined as pointer:           " << (*p2.v).elt << std::endl; 
+//    std::cout << *p2 << std::endl; // no match for operator*
+    Position3 p3 = Position3(n);
+    std::cout << "Accessing class member defined as value:                          " << p3.v.elt << std::endl;
     return 0;
 }
