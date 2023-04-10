1aa49f58ef5606b9fbf34d1d17387258d27537e2
valteem <val.teem.scn@gmail.com>
2023-04-10 20:30:31 +0300

    rvalue reference exercise (WIP)

---
+++ other/references.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    int a1 = 1;
+    int a2 = 2;
+
+    int&& r = a1 + a2; // rvalue reference
+    r++;
+    std::cout << r << std::endl;
+
+    int* p = &r; // what is this if not pointer to reference?
+    std::cout << &r << " " << p << " " << *p << std::endl;
+
+    *p++;
+    std::cout << &r << " " << p << " " << *p << std::endl;
+
+}
