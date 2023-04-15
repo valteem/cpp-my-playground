5bc668c8866cb7ef182f3cf12435870d984f95c0
valteem <val.teem.scn@gmail.com>
2023-04-15 21:09:15 +0300

    basic vtable layout example using gimple - minor updates

--- other/vtable_layout.cpp
+++ other/vtable_layout.cpp
@@ -1,25 +1,33 @@
+#include <iostream>
+
 class A {
     public:
     A(){};
     virtual ~A(){};
-    virtual void V(){};
-    void N(){};
+    virtual int V(){
+        return 1;
+    };
+    int N(){
+        return 2;
+    };
 };
 
 class B : public A {
     public:
     B(){A();};
     ~B(){};
-    void V() {};
-    void N() {};
+    int V() {
+        return 11;
+    };
+    int N() {
+        return 12;
+    };
 };
 
 int main() {
     A a;
     B b;
-    a.V();
-    a.N();
-    b.V();
-    b.N();
+    A& ar = b;
+    std::cout << a.V() <<+ " "  << a.N() << " " << b.V() << " " << b.N() <<  " " << ar.V() << " " << ar.N() << "\n";
     return 0;
 }
