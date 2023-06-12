bfe0aba28e1db29581553830798ed969bcecd52c
valteem <val.teem.scn@gmail.com>
2023-06-12 16:25:39 +0300

    more elaborations in pointer-to-array example

--- other/pointer_to_array.cpp
+++ other/pointer_to_array.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 int main() {
-    int r[2] {1, 2}; // array of int
-    int *p[2];       // array of pointers to int
-    int (*q)[2];     // pointer to array of int
+    int r[3] {1, 2, 3}; // array of int
+    int *p[3];          // array of pointers to int
+    int (*q)[3];        // pointer to array of int
     
     p[0] = &r[0];
     p[1] = &r[1];
     q = &r;
     std::cout << p << " " << *p << " " << "\n";
-    std::cout << *p[0] << " " << *p[1] << " " << p[0] << " " << p[1] << "\n";
+    std::cout << *p[0] << " " << *p[1] << " " << p[0] << " " << p[1] << " " << (p[1] - p[0]) << "\n";
     std::cout << q << " " << *q << "\n";
-    std::cout << *q[0] << " " << (*q)[1] << " " << q[0] << " " << q[1] << "\n";
+    std::cout << *q[0] << " " << (*q)[1] << " " << (*q)[2] << " " << q[0] << " " << q[1] << " " << (q[1] - q[0]) << "\n";
 
     return 1;
 }
