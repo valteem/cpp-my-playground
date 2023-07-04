631424cee5ec10bdc1da5e03e6ccde302e3c07b5
valteem <val.teem.scn@gmail.com>
2023-07-04 17:12:46 +0300

    fold expression - associativity example

--- other/fold_expression_sum.cpp
+++ other/fold_expression_sum.cpp
@@ -5,6 +5,18 @@ auto sum(Values const&... values) {
     return (values + ...);
 }
 
+template<typename... Values>
+auto subRight(Values const&... values) {
+    return (values - ...);
+}
+
+template<typename... Values>
+auto subLeft(Values const&... values) {
+    return (... - values);
+}
+
 int main() {
     std::cout << sum(1, 2, 3, 4) << std::endl;
+    std::cout << subRight(1, 2, 3) << std::endl;  // 1 - (2 - 3) = 2
+    std::cout << subLeft(1, 2, 3) << std::endl; // (1 - 2) - 3 = -4
 }
