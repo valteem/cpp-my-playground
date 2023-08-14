8f0db7a6746d39f9f3b017c4565b7f55a5012266
valteem <val.teem.scn@gmail.com>
2023-08-14 12:36:49 +0300

    added Named C'tor to private c'tor example

--- other/private_ctor.cpp
+++ other/private_ctor.cpp
@@ -1,6 +1,8 @@
-// https://www.geeksforgeeks.org/can-constructor-private-cpp/
-
+#include <cmath> // std::cos(), std::sin()
 #include <iostream>
+#include <utility>
+
+// https://www.geeksforgeeks.org/can-constructor-private-cpp/
 
 class A {
     int a;
@@ -19,9 +21,45 @@ class B {
     }
 };
 
+// https://stackoverflow.com/a/6568587
+// Named C'tors
+
+class Point {
+    public:
+    static Point Polar(double, double);     // 'static' eliminates ambiguity in c'tor overloading
+    static Point Cartesian(double, double);
+    std::pair<double, double> Coord() ;
+    private:
+    Point(double, double);
+    double x_, y_; // Data members of classes, both static and non-static, are named like ordinary nonmember variables, but with a trailing underscore 
+};
+
+inline std::pair<double, double> Point::Coord() {
+    return std::make_pair(x_, y_);
+}
+
+inline Point::Point(double x, double y): x_(x), y_(y) {};
+
+inline Point Point::Cartesian(double x, double y) {
+    return Point(x, y);
+};
+
+inline Point Point::Polar(double x, double y) {
+    return Point(x * std::cos(y), x * std::sin(y));
+};
+
+
+
 int main() {
     B b;
     A a = b.create_a(1);
     std::cout << get_a(a) << "\n";
+
+    auto p1 = Point::Cartesian(1, 1);
+    std::cout << p1.Coord().first << " " << p1.Coord().second << std::endl;
+    auto p2 = Point::Polar(1, 1);
+    std::cout << p2.Coord().first << " " << p2.Coord().second << std::endl;
+    
+    return 0;
 
 }
