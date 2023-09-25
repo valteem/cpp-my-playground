17defe9e85634d2e2d7536cd7b5342c690cf0ce8
valteem <val.teem.scn@gmail.com>
2023-09-25 11:49:28 +0300

    template specialization example

--- other/template_specialization.cpp
+++ other/template_specialization.cpp
@@ -1,1 +1,28 @@
 // https://github.com/boostorg/algorithm/blob/32c5a6327cfdca5d41ce0f1d8849b811886daa2f/include/boost/algorithm/searching/detail/bm_traits.hpp#L36
+
+#include <iostream>
+
+template<bool>class A;
+
+template<>
+class A<true> {
+    public:
+    A() {
+        std::cout << "true" << std::endl;
+    }
+};
+
+template<>
+class A<false> {
+    public:
+    A() {
+        std::cout << "false" << std::endl;
+    }
+};
+
+int main() {
+
+    A a1 = A<true>();
+    A a2 = A<false>();
+
+}
