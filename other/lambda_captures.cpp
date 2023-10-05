bad34ff2f79eb1cf5b6acc230566967b4d94c8dd
valteem <val.teem.scn@gmail.com>
2023-10-05 17:45:07 +0300

    using reference for lambda capture

--- other/lambda_captures.cpp
+++ other/lambda_captures.cpp
@@ -14,12 +14,24 @@ int main() {
         return count;
     };
 
-    std::string s{"aaabb"};
+    std::string s{"aaabbc"};
 
     std::cout << counter(s) << std::endl; // returns 3
 
     symbol_to_count = 'b';
     std::cout << counter(s) << std::endl; // returns 3, new assignment for symbol_to_count ignored
+
+    auto counter_ref = [&symbol_to_count] (const std::string str) {
+        size_t index{}, count{};
+        while(str[index]) {
+            if (str[index] == symbol_to_count) count++;
+            index++;
+        };
+        return count;
+    };
+    std::cout << counter_ref(s) << std::endl; // returns 2
+    symbol_to_count = 'c';
+    std::cout << counter_ref(s) << std::endl; // returns 1, new aasignment accepted
 
     return 0;
 }
