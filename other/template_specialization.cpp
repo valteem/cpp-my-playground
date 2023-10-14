8a5e7261835bd865c25670b85636349be55230b2
valteem <val.teem.scn@gmail.com>
2023-10-14 15:08:42 +0300

    partial specialization (as opposed to full)

--- other/template_specialization.cpp
+++ other/template_specialization.cpp
@@ -4,7 +4,7 @@
 
 template<bool>class A;
 
-template<>
+template<>  // template <> introduces full specializations https://stackoverflow.com/a/25308774
 class A<true> {
     public:
     A() {
@@ -20,9 +20,29 @@ class A<false> {
     }
 };
 
+// partial specialization
+template<typename X, int I> class B; //primary template
+template<typename X>
+class B<X, 1> {
+    public:
+    B() {std::cout << "1" << std::endl;}
+};
+template<typename X>
+class B<X, 2> {
+    public:
+    B() {std::cout << "2" << std::endl;}
+};
+
 int main() {
 
+// full specialization
     A a1 = A<true>();
     A a2 = A<false>();
+
+// partial specialization
+    B b1 = B<int,1>();
+    B b2 = B<char*, 2>();
+
+    return 0;
 
 }
