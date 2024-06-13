b7ddcec1db3a6a29f4d8e1ec14035b51bc97bbca
valteem <val.teem.scn@gmail.com>
2024-06-13 12:05:02 +0300

    aggregate initialization

--- layouts/testing/tests/using_intro_test.cc
+++ layouts/testing/tests/using_intro_test.cc
@@ -5,9 +5,15 @@
 namespace usingIntro {
 
     TEST(using_intro, using_into) {
-            B* b = new B(1,2);
-            EXPECT_EQ(b->x, 1);
-            EXPECT_EQ(b->y, 2);
+        B* b = new B(1,2);
+        EXPECT_EQ(b->x, 1);
+        EXPECT_EQ(b->y, 2);
     };
+
+    TEST(using_intro, ctor) {
+        B b {1, 2}; // using curly braces is only allowed if c'tor has initialization list
+        EXPECT_EQ(b.x, 1);
+        EXPECT_EQ(b.y, 2);
+    }
 
 }
