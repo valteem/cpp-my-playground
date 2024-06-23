5c215a6e022c83028bbeefeeae76cb15e210d15f
valteem <val.teem.scn@gmail.com>
2024-06-23 19:14:13 +0300

    elaborate on implicit vs explicit destructor call

--- other/implicit_destructor_call.cpp
+++ other/implicit_destructor_call.cpp
@@ -8,11 +8,18 @@ struct A {
     }
 };
 
+// Object on stack are destroyed implicitly when going out of scope
+// Objects on heap need to be destroyed explicitly
+
 int main() {
-    A a{1};
+    A a{0};
+    A* pa = new A{1};
     // add subscope
     {
         A a{2};
+        A* pa = new A{3};
+        delete pa;
     }
+    delete pa; 
     return 0;
 }
