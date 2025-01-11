d8e68e2fd51dc31dae559e7b7462037c89f7e636
valteem <val.teem.scn@gmail.com>
2025-01-11 17:28:22 +0300

    other: do {} while(0)

---
+++ other/do_while_0.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    do {
+        if (!(false)) {
+            std::cout << "just a message" << std::endl;
+        }
+    } while(0);
+
+    return 0;
+
+}
