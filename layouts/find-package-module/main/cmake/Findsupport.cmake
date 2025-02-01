c1a738529e54b70a167fe823159fc5394134b606
valteem <val.teem.scn@gmail.com>
2025-02-01 17:38:43 +0700

    layouts/find-package-module: fix find_path(), add find_library()

--- layouts/find-package-module/main/cmake/Findsupport.cmake
+++ layouts/find-package-module/main/cmake/Findsupport.cmake
@@ -1,8 +1,34 @@
+message(STATUS "Module file found")
+
 message(STATUS "CMAKE_SOURCE_DIR ${CMAKE_SOURCE_DIR}")
 message(STATUS "CMAKE_CURRENT_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}")
 
+set(SUPPORT_ROOT_DIR "${CMAKE_SOURCE_DIR}/../support")
+
 find_path(support_INCLUDE_DIR
 NAMES "support.h"
-PATHS "${CMAKE_SOURCE_DIR}/../support/include")
+#PATHS "${CMAKE_SOURCE_DIR}/../support/include")
+PATHS "${SUPPORT_ROOT_DIR}"
+PATH_SUFFIXES "include"
+NO_DEFAULT_PATH)
 
-message(STATUS "Module file found")
+find_library(support_LIB
+NAMES "libsupport.a"
+#PATHS "${CMAKE_SOURCE_DIR}/../support/lib")
+PATHS "${SUPPORT_ROOT_DIR}"
+PATH_SUFFIXES "lib"
+NO_DEFAULT_PATH)
+
+include(FindPackageHandleStandardArgs)
+# shows only first required variable:
+#
+# if (${_NAME}_FOUND)
+# FIND_PACKAGE_MESSAGE(${_NAME} "Found ${_NAME}: ${${_FIRST_REQUIRED_VAR}} ${VERSION_MSG} ${COMPONENT_MSG}" "${DETAILS}")
+#
+find_package_handle_standard_args(support DEFAULT_MSG support_INCLUDE_DIR support_LIB)
+
+if (support_FOUND)
+    message(STATUS "found support module: ${support_INCLUDE_DIR} ${support_LIB}")
+else()
+    message(STATUS "support module not found")
+endif()
---
+++ layouts/find-package-module/support/src/support.cc
@@ -0,0 +1,7 @@
+#include "support.h"
+
+namespace support {
+    int add2(int a) {
+        return add(a, 2);
+    }
+} // namespace support
